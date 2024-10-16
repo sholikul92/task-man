401a30c2eac1ae25a32555ae18b34b837c0e2bbc
sholikul <sholikulardian@gmail.com>
2024-10-16 15:40:38 +0700

    create controller user

---
+++ src/users/user.controller.js
@@ -0,0 +1,52 @@
+const user = new User()
+const btnRegister = document.getElementById('btn-register');
+const btnLogin = document.getElementById('btn-login')
+
+btnRegister?.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const username = document.getElementById('username').value;
+
+  const users = user.getUsers();
+    
+  const userExist = users.find(user => user.username === username);
+
+  if (username.length === 0) {
+    throwErrorMessage("Username tidak boleh kosong!")
+  } else {
+    if (userExist) {
+      throwErrorMessage("Username sudah terdaftar!")
+    } else {
+      const result = userRegist.saveUser({username})
+      if (result.success) {
+        window.location.href = "../../pages/login.html"
+      }
+    }
+  }
+})
+
+btnLogin?.addEventListener('click', (e) => {
+  e.preventDefault();
+  
+  const username = document.getElementById('username').value;
+
+  const users = user.getUsers();
+  const userExists = users.find(user => user.username === username);
+
+  if (username.length === 0) {
+    throwErrorMessage("Username tidak boleh kosong!")
+  } else {
+    if (userExists) {
+      window.location.href = "../../pages/dashboard.html";
+    } else {
+      throwErrorMessage("Username tidak terdaftar!")
+    }
+  }
+})
+
+const throwErrorMessage = (text) => {
+  document.getElementById('username').classList.add('border-red-500');
+  document.getElementById('username').classList.replace('focus:outline-blue-500', 'focus:outline-red-500');
+  document.getElementById('alert-failed').classList.remove('hidden')
+  document.getElementById('alert-failed').textContent = text
+}
